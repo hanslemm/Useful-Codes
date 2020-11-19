0343ec415dcb5459954cfec46729071010dbb2cd
Hans Lemm <hcrlemm@gmail.com>
2020-11-19 14:41:05 -0300

    Initial Commit
    Including initial file

---
+++ Python_Decorators.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, abort
+import functools
+app = Flask(__name__)
+"""
+This decorator validates JSON according to the
+arguments you pass into it
+"""
+def validate_json(*expected_args):                  # 1
+    def decorator_validate_json(func):
+        @functools.wraps(func)
+        def wrapper_validate_json(*args, **kwargs):
+            json_object = request.get_json()
+            for expected_arg in expected_args:      # 2
+                if expected_arg not in json_object:
+                    abort(400)
+            return func(*args, **kwargs)
+        return wrapper_validate_json
+    return decorator_validate_json
